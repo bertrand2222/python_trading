a9bd9199bfc08d4b27d913fc34939aed67a9846f
Bertrand K <bertrand.khayat69@gmail.com>
2022-02-17 22:01:25 +0100

    make dataset

--- exec_multi.py
+++ exec_multi.py
@@ -2,36 +2,40 @@ import pandas as pd
 # from pandas_datareader import data
 import matplotlib.pyplot as plt
 import datetime as dt
-import urllib.request, json
 import os
 import tensorflow as tf# This code has been tested with TensorFlow 1.6
-from sklearn.preprocessing import MinMaxScaler
+#from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import yfinance as yf
 
 WIN_SIZE = 40
 symbol ="CA.PA"
 
-#def get_train_test_data(symbol):
+tk = yf.Ticker(symbol)
+
+#earning per share
+eps = tk.info['trailingEps']
+
 #df = yf.download(symbol, period="20y", interval = "1d").bfill()
 #df.to_pickle("df.pkl")
 df = pd.read_pickle("df.pkl")
 
 val_cols = ['Open' , 'High', 'Low', 'Close']
-#plot_cols = ['High', 'Low', 'Volume']
+input_cols = val_cols + ["Volume"]
+
 #plot_features = df[plot_cols]
 ##plot_features.index = date_time
 #_ = plot_features.plot(subplots=True)
-
 
 #plt.show()
 
 column_indices = {name: i for i, name in enumerate(df.columns)}
 
 n = len(df)
-train_df = df[0:int(n*0.7)].copy()
-val_df = df[int(n*0.7):int(n*0.9)].copy()
-test_df = df[int(n*0.9):].copy()
+
+train_df = df[input_cols][0:int(n*0.7)].copy()
+val_df = df[input_cols][int(n*0.7):int(n*0.9)].copy()
+test_df = df[input_cols][int(n*0.9):].copy()
 
 num_features = df.shape[1]
 
@@ -89,325 +93,76 @@ class WindowGenerator():
         f'Label indices: {self.label_indices}',
         f'Label column name(s): {self.label_columns}'])
 
+  def split_window(self, features):
+    inputs = features[:, self.input_slice, :]
+    labels = features[:, self.labels_slice, :]
+    if self.label_columns is not None:
+      labels = tf.stack(
+          [labels[:, :, self.column_indices[name]] for name in self.label_columns], axis=-1)
+
+    # Slicing doesn't preserve static shape information, so set the shapes
+    # manually. This way the `tf.data.Datasets` are easier to inspect.
+    inputs.set_shape([None, self.input_width, None])
+    labels.set_shape([None, self.label_width, None])
+
+    return inputs, labels
+
+  def make_dataset(self, data):
+    data = np.array(data, dtype=np.float32)
+    ds = tf.keras.utils.timeseries_dataset_from_array(
+        data=data,
+        targets=None,
+        sequence_length=self.total_window_size,
+        sequence_stride=1,
+        shuffle=True,
+        batch_size=32,)
+
+    ds = ds.map(self.split_window)
+
+    return ds
+
+  @property
+  def train(self):
+    return self.make_dataset(self.train_df)
+
+  @property
+  def val(self):
+    return self.make_dataset(self.val_df)
+
+  @property
+  def test(self):
+    return self.make_dataset(self.test_df)
+
+  @property
+  def example(self):
+    """Get and cache an example batch of `inputs, labels` for plotting."""
+    result = getattr(self, '_example', None)
+    if result is None:
+      # No example batch was found, so get one from the `.train` dataset
+      result = next(iter(self.train))
+      # And cache it for next time
+      self._example = result
+    return result
+
 
 w2 = WindowGenerator(input_width=6, label_width=1, shift=1, label_columns=['Close'])
 print(w2)
 
-stop
 
-ratio_test_train = 1/12
-len_data = df.shape[0]
-sep_train_test = int(len_data * (1 - ratio_test_train))
-price = df["Close"].to_numpy()
+# Stack three slices, the length of the total window.
+example_window = tf.stack([np.array(train_df[:w2.total_window_size]),
+                           np.array(train_df[100:100+w2.total_window_size]),
+                           np.array(train_df[200:200+w2.total_window_size])])
 
-train_data = price[:sep_train_test].reshape(-1,1)
-test_data = price[sep_train_test:].reshape(-1,1)
+example_inputs, example_labels = w2.split_window(example_window)
 
-scaler = MinMaxScaler()
-smoothing_window_size = int(len_data * ratio_test_train * 2) + 1
-for di in range(0,sep_train_test ,smoothing_window_size):
-    scaler.fit(train_data[di:di+smoothing_window_size,:])
-    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+print('All shapes are: (batch, time, features)')
+print(f'Window shape: {example_window.shape}')
+print(f'Inputs shape: {example_inputs.shape}')
+print(f'Labels shape: {example_labels.shape}')
 
-train_data = train_data.reshape(-1)
-test_data = scaler.transform(test_data).reshape(-1)
 
-#plt.plot(range(0, train_data.shape[0]), train_data)
 
-# Now perform exponential moving average smoothing
-# So the data will have a smoother curve than the original ragged data
-EMA = 0.0
-gamma = 0.1
-for ti in range(sep_train_test):
-    EMA = gamma*train_data[ti] + (1-gamma)*EMA
-    train_data[ti] = EMA
-#plt.plot(range(len(train_data)), train_data)
-    #return train_data, test_data
 
 
-#train_data, test_data = get_train_test_data(symbol)
-len_train_data = len(train_data)
-len_price = len_train_data + len(test_data)
-print("len train data " + str(len_train_data))
-all_mid_data = np.concatenate([train_data,test_data],axis=0)
-#dg = DataGeneratorSeq(train_data,5,5)
-#u_data, u_labels = dg.unroll_batches()
 
-#for ui,(dat,lbl) in enumerate(zip(u_data,u_labels)):
-    #print('\n\nUnrolled index %d'%ui)
-    #dat_ind = dat
-    #lbl_ind = lbl
-    #print('\tInputs: ',dat )
-    #print('\n\tOutput:',lbl)
-
-#plt.show()
-
-D = 1 # Dimensionality of the data. Since your data is 1-D this would be 1
-num_unrollings = 25 # Number of time steps you look into the future.
-batch_size = 500 # Number of samples in a batch
-num_nodes = [200,200,150] # Number of hidden nodes in each layer of the deep LSTM stack we're using
-n_layers = len(num_nodes) # number of layers
-dropout = 0.2 # dropout amount
-
-tf.reset_default_graph()
-
-# Input data.
-train_inputs, train_outputs = [],[]
-
-# You unroll the input over time defining placeholders for each time step
-for ui in range(num_unrollings):
-    train_inputs.append(tf.placeholder(tf.float32, shape=[batch_size,D],name='train_inputs_%d'%ui))
-    train_outputs.append(tf.placeholder(tf.float32, shape=[batch_size,1], name = 'train_outputs_%d'%ui))
-
-lstm_cells = [
-    tf.keras.layers.LSTMCell(units=num_nodes[li],
-                            dropout = dropout
-                           )
- for li in range(n_layers)]
-
-#drop_lstm_cells = [tf.nn.rnn_cell.DropoutWrapper(
-    #lstm, input_keep_prob=1.0,output_keep_prob=1.0-dropout, state_keep_prob=1.0-dropout
-#) for lstm in lstm_cells]
-#drop_multi_cell = tf.nn.rnn_cell.MultiRNNCell(drop_lstm_cells)
-#multi_cell = tf.nn.rnn_cell.MultiRNNCell(lstm_cells)
-rnn_layer = tf.keras.layers.RNN(lstm_cells, return_sequences = True,
-                                return_state = True, time_major = True)
-
-w = tf.get_variable('w',shape=[num_nodes[-1], 1], initializer=tf.initializers.glorot_normal())
-b = tf.get_variable('b',initializer=tf.random_uniform([1],-0.1,0.1))
-
-# Create cell state and hidden state variables to maintain the state of the LSTM
-c, h = [],[]
-initial_state = []
-for li in range(n_layers):
-  c.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
-  h.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
-  initial_state.append(tf.nn.rnn_cell.LSTMStateTuple(c[li], h[li]))
-
-# Do several tensor transofmations, because the function dynamic_rnn requires the output to be of
-# a specific format. Read more at: https://www.tensorflow.org/api_docs/python/tf/nn/dynamic_rnn
-all_inputs = tf.concat([tf.expand_dims(t,0) for t in train_inputs],axis=0)
-
-# all_outputs is [seq_length, batch_size, num_nodes]
-#all_lstm_outputs, state = tf.nn.dynamic_rnn(
-    #drop_multi_cell, all_inputs, initial_state=tuple(initial_state),
-    #time_major = True, dtype=tf.float32)
-
-all_lstm_outputs, *state = rnn_layer(all_inputs, initial_state = tuple(initial_state))
-
-#tf.saved_model.save(rnn_layer, 'savedmodel.tfmod')
-
-all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings,num_nodes[-1]])
-all_outputs = tf.nn.xw_plus_b(all_lstm_outputs,w,b)
-split_outputs = tf.split(all_outputs,num_unrollings,axis=0)
-
-
-# When calculating the loss you need to be careful about the exact form, because you calculate
-# loss of all the unrolled steps at the same time
-# Therefore, take the mean error or each batch and get the sum of that over all the unrolled steps
-
-print('Defining training Loss')
-loss = 0.0
-with tf.control_dependencies([tf.assign(c[li], state[li][0]) for li in range(n_layers)]+
-                             [tf.assign(h[li], state[li][1]) for li in range(n_layers)]):
-  for ui in range(num_unrollings):
-    loss += tf.reduce_mean(0.5*(split_outputs[ui]-train_outputs[ui])**2)
-
-print('Learning rate decay operations')
-global_step = tf.Variable(0, trainable=False)
-inc_gstep = tf.assign(global_step,global_step + 1)
-tf_learning_rate = tf.placeholder(shape=None,dtype=tf.float32)
-tf_min_learning_rate = tf.placeholder(shape=None,dtype=tf.float32)
-
-learning_rate = tf.maximum(
-    tf.train.exponential_decay(tf_learning_rate, global_step, decay_steps=1, decay_rate=0.5, staircase=True),
-    tf_min_learning_rate)
-
-# Optimizer.
-print('TF Optimization operations')
-optimizer = tf.train.AdamOptimizer(learning_rate)
-gradients, v = zip(*optimizer.compute_gradients(loss))
-gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-optimizer = optimizer.apply_gradients(zip(gradients, v))
-
-print('\tAll done')
-
-print('Defining prediction related TF functions')
-
-sample_inputs = tf.placeholder(tf.float32, shape=[1,D])
-
-# Maintaining LSTM state for prediction stage
-sample_c, sample_h, initial_sample_state = [],[],[]
-for li in range(n_layers):
-  sample_c.append(tf.Variable(tf.zeros([1, num_nodes[li]]), trainable=False))
-  sample_h.append(tf.Variable(tf.zeros([1, num_nodes[li]]), trainable=False))
-  initial_sample_state.append(tf.nn.rnn_cell.LSTMStateTuple(sample_c[li],sample_h[li]))
-
-reset_sample_states = tf.group(*[tf.assign(sample_c[li],tf.zeros([1, num_nodes[li]])) for li in range(n_layers)],
-                               *[tf.assign(sample_h[li],tf.zeros([1, num_nodes[li]])) for li in range(n_layers)])
-
-sample_outputs, *sample_state = rnn_layer(tf.expand_dims(sample_inputs,0),
-                                   initial_state=tuple(initial_sample_state),)
-
-with tf.control_dependencies([tf.assign(sample_c[li],sample_state[li][0]) for li in range(n_layers)]+
-                              [tf.assign(sample_h[li],sample_state[li][1]) for li in range(n_layers)]):
-  sample_prediction = tf.nn.xw_plus_b(tf.reshape(sample_outputs,[1,-1]), w, b)
-
-print('\tAll done')
-
-epochs = 30
-valid_summary = 1 # Interval you make test predictions
-n_predict_once = 25 # Number of steps you continously predict for
-train_seq_length = train_data.size # Full length of the training data
-
-train_mse_ot = [] # Accumulate Train losses
-test_mse_ot = [] # Accumulate Test loss
-predictions_over_time = [] # Accumulate predictions
-
-session = tf.InteractiveSession()
-
-tf.global_variables_initializer().run()
-
-# Used for decaying learning rate
-loss_nondecrease_count = 0
-loss_nondecrease_threshold = 2 # If the test error hasn't increased in this many steps, decrease learning rate
-
-print('Initialized')
-average_loss = 0
-
-# Define data generator
-data_gen = DataGeneratorSeq(train_data,batch_size,num_unrollings)
-
-x_axis_seq = []
-
-# Points you start your test predictions from
-test_points_seq = np.arange(len_train_data ,len_price - n_predict_once, n_predict_once).tolist()
-
-for ep in range(epochs):
-
-    # ========================= Training =====================================
-    for step in range(train_seq_length//batch_size):
-
-        u_data, u_labels = data_gen.unroll_batches()
-
-        feed_dict = {}
-        for ui,(dat,lbl) in enumerate(zip(u_data,u_labels)):
-            feed_dict[train_inputs[ui]] = dat.reshape(-1,1)
-            feed_dict[train_outputs[ui]] = lbl.reshape(-1,1)
-
-        feed_dict.update({tf_learning_rate: 0.0001, tf_min_learning_rate:0.000001})
-
-        _, l = session.run([optimizer, loss], feed_dict=feed_dict)
-
-        average_loss += l
-
-    # ============================ Validation ==============================
-    if (ep+1) % valid_summary == 0:
-
-      average_loss = average_loss/(valid_summary*(train_seq_length//batch_size))
-
-      # The average loss
-      if (ep+1)%valid_summary==0:
-        print('Average loss at step %d: %f' % (ep+1, average_loss))
-
-      train_mse_ot.append(average_loss)
-
-      average_loss = 0 # reset loss
-      predictions_seq = []
-      mse_test_loss_seq = []
-
-      # ===================== Updating State and Making Predicitons ========================
-      for w_i in test_points_seq:
-        mse_test_loss = 0.0
-        our_predictions = []
-
-        if (ep+1)-valid_summary==0:
-          # Only calculate x_axis values in the first validation epoch
-          x_axis=[]
-
-        # Feed in the recent past behavior of stock prices
-        # to make predictions from that point onwards
-        for tr_i in range(w_i-num_unrollings+1,w_i-1):
-          current_price = all_mid_data[tr_i]
-          feed_dict[sample_inputs] = np.array(current_price).reshape(1,1)
-          _ = session.run(sample_prediction,feed_dict=feed_dict)
-
-        feed_dict = {}
-
-        current_price = all_mid_data[w_i-1]
-
-        feed_dict[sample_inputs] = np.array(current_price).reshape(1,1)
-
-        # Make predictions for this many steps
-        # Each prediction uses previous prediciton as it's current input
-        for pred_i in range(n_predict_once):
-
-          pred = session.run(sample_prediction,feed_dict=feed_dict)
-          our_predictions.append(np.asscalar(pred))
-          feed_dict[sample_inputs] = np.asarray(pred).reshape(-1,1)
-
-          if (ep+1)-valid_summary==0:
-            # Only calculate x_axis values in the first validation epoch
-            x_axis.append(w_i+pred_i)
-
-          mse_test_loss += 0.5*(pred-all_mid_data[w_i+pred_i])**2
-
-        session.run(reset_sample_states)
-
-        predictions_seq.append(np.array(our_predictions))
-
-        mse_test_loss /= n_predict_once
-        mse_test_loss_seq.append(mse_test_loss)
-
-        if (ep+1)-valid_summary==0:
-          x_axis_seq.append(x_axis)
-
-      current_test_mse = np.mean(mse_test_loss_seq)
-
-      # Learning rate decay logic
-      if len(test_mse_ot)>0 and current_test_mse > min(test_mse_ot):
-          loss_nondecrease_count += 1
-      else:
-          loss_nondecrease_count = 0
-
-      if loss_nondecrease_count > loss_nondecrease_threshold :
-            session.run(inc_gstep)
-            loss_nondecrease_count = 0
-            print('\tDecreasing learning rate by 0.5')
-
-      test_mse_ot.append(current_test_mse)
-      print('\tTest MSE: %.5f'%current_test_mse)
-      predictions_over_time.append(predictions_seq)
-      print('\tFinished Predictions')
-
-best_prediction_epoch = test_mse_ot.index(min(test_mse_ot)) # replace this with the epoch that you got the best results when running the plotting code
-
-plt.figure(figsize = (18,18))
-plt.subplot(2,1,1)
-plt.plot(range(df.shape[0]),all_mid_data,color='b')
-
-# Plotting how the predictions change over time
-# Plot older predictions with low alpha and newer predictions with high alpha
-start_alpha = 0.25
-alpha  = np.arange(start_alpha,1.1,(1.0-start_alpha)/len(predictions_over_time[::3]))
-for p_i,p in enumerate(predictions_over_time[::3]):
-    for xval,yval in zip(x_axis_seq,p):
-        plt.plot(xval,yval,color='r',alpha=alpha[p_i])
-
-plt.title('Evolution of Test Predictions Over Time',fontsize=18)
-plt.xlabel('Date',fontsize=18)
-plt.ylabel('Mid Price',fontsize=18)
-plt.xlim(len_train_data,len_price)
-
-plt.subplot(2,1,2)
-
-# Predicting the best test prediction you got
-plt.plot(range(df.shape[0]),all_mid_data,color='b')
-for xval,yval in zip(x_axis_seq,predictions_over_time[best_prediction_epoch]):
-    plt.plot(xval,yval,color='r')
-
-plt.title('Best Test Predictions Over Time',fontsize=18)
-plt.xlabel('Date',fontsize=18)
-plt.ylabel('Mid Price',fontsize=18)
-plt.xlim(len_train_data,len_price)
-plt.show()
