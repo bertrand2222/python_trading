fa3197a49972d155db8fdd0fa59cfa83c023cae6
Bertrand K <bertrand.khayat69@gmail.com>
2022-02-17 19:38:19 +0100

    generateur fenetre

--- exec_multi.py
+++ exec_multi.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import yfinance as yf
 
+WIN_SIZE = 40
 symbol ="CA.PA"
 
 #def get_train_test_data(symbol):
@@ -16,6 +17,7 @@ symbol ="CA.PA"
 #df.to_pickle("df.pkl")
 df = pd.read_pickle("df.pkl")
 
+val_cols = ['Open' , 'High', 'Low', 'Close']
 #plot_cols = ['High', 'Low', 'Volume']
 #plot_features = df[plot_cols]
 ##plot_features.index = date_time
@@ -27,12 +29,69 @@ df = pd.read_pickle("df.pkl")
 column_indices = {name: i for i, name in enumerate(df.columns)}
 
 n = len(df)
-train_df = df[0:int(n*0.7)]
-val_df = df[int(n*0.7):int(n*0.9)]
-test_df = df[int(n*0.9):]
+train_df = df[0:int(n*0.7)].copy()
+val_df = df[int(n*0.7):int(n*0.9)].copy()
+test_df = df[int(n*0.9):].copy()
 
 num_features = df.shape[1]
 
+#Normalisation
+for dfi in [train_df, val_df, test_df] :
+  # calculate Simple Moving Average with 20 days window
+  sma = dfi['Close'].rolling(window=WIN_SIZE).mean()
+  vrmean = dfi['Volume'].rolling(window = WIN_SIZE).mean()
+  # calculate the standar deviation
+  rstd = dfi['Close'].rolling(window=WIN_SIZE).std()
+  vrstd = dfi['Volume'].rolling(window = WIN_SIZE).std()
+
+  dfi[val_cols] = dfi[val_cols].sub(sma, axis = 0)
+  dfi[val_cols]  = dfi[val_cols].divide(rstd, axis = 0)
+
+  dfi['Volume'] = dfi['Volume'].sub(vrmean, axis = 0)
+  dfi['Volume'] = dfi['Volume'].divide(vrstd, axis = 0)
+
+
+class WindowGenerator():
+  def __init__(self, input_width, label_width, shift,
+               train_df=train_df, val_df=val_df, test_df=test_df,
+               label_columns=None):
+    # Store the raw data.
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices.
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters.
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+
+
+w2 = WindowGenerator(input_width=6, label_width=1, shift=1, label_columns=['Close'])
+print(w2)
 
 stop
 
